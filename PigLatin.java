844659593f6c51a08f62e954ecb9fc2631e43ae6
Sarah Leong <sleong20@stuy.edu>
2021-01-04 15:47:06 -0500

    fixed for words that start with vowels

--- PigLatin.java
+++ PigLatin.java
@@ -1,14 +1,15 @@
 public class PigLatin {
     public static String pigLatinSimple(String s) {
-        String translated = "";
+        String translated = s;
         if (s.charAt(0) != 'a' && s.charAt(0) != 'e' &&
         s.charAt(0) != 'i' && s.charAt(0) != 'o' && s.charAt(0) != 'u') {
-            translated = s.substring(1) + s.charAt(0);
+            translated = s.substring(1) + s.charAt(0) + "ay";
+        } else {
+            translated += "hay";
         }
-        translated += "ay";
         return translated;
     }
     public static void main(String[] args) {
-        System.out.println(pigLatinSimple("pie"));
+        System.out.println(pigLatinSimple("aaron"));
     }
 }
