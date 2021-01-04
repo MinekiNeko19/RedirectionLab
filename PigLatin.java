8984b05b1ef36bb3cb9fd70fdb297838de9cc937
Sarah Leong <sleong20@stuy.edu>
2021-01-04 13:22:07 -0500

    added and tested pigLatinSimple

--- PigLatin.java
+++ PigLatin.java
@@ -1,3 +1,14 @@
 public class PigLatin {
-    
+    public static String pigLatinSimple(String s) {
+        String translated = "";
+        if (s.charAt(0) != 'a' && s.charAt(0) != 'e' &&
+        s.charAt(0) != 'i' && s.charAt(0) != 'o' && s.charAt(0) != 'u') {
+            translated = s.substring(1) + s.charAt(0);
+        }
+        translated += "ay";
+        return translated;
+    }
+    public static void main(String[] args) {
+        System.out.println(pigLatinSimple("pie"));
+    }
 }
