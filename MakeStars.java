57c0fe83150d66e73215e6cebd1f2c907aa93428
Sarah Leong <sleong20@stuy.edu>
2021-01-04 13:10:35 -0500

    main converts words into the appropriate number of stars

--- MakeStars.java
+++ MakeStars.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class MakeStars {
-    
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        while (n.hasNextLine()) {
+            Scanner line = new Scanner(n.nextLine());
+            while (line.hasNext()) {
+                String word = line.next();
+                for (int i = 0; i < word.length(); i++) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
 }
